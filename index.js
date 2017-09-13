c4766a57f53954caaf15e01cdfcb9c78bf591049
bbonar <brettbonar@gmail.com>
2017-09-12 19:28:35 -0600

    working-ish

--- index.js
+++ index.js
@@ -5,29 +5,68 @@ const dgram = require("dgram");
 const inquirer = require("inquirer");
 const _ = require("lodash");
 const q = require("q");
-const Rx = require("rx-lite");
 
+const BYTE = "byte";
 const INT = "int";
 const STRING = "string";
 const MESSAGE_TYPES = {
   startGame: 1,
-  gameDef: 2
+  gameDef: 2,
+  guess: 3,
+  answer: 4,
+  getHint: 5,
+  hint: 6,
+  exit: 7,
+  ack: 8,
+  error: 9,
+  heartbeat: 10
 };
 
 var commands = {};
 var handlers = {};
+var gameDef = {};
 
 var parsers = {
-  startGame: { // Start Game
+  startGame: {
     aNum: STRING,
     lastName: STRING,
     firstName: STRING,
     alias: STRING
   },
-  gameDef: { // GameDef
+  gameDef: {
     id: INT,
     hint: STRING,
     definition: STRING
+  },
+  guess: {
+    id: INT,
+    guess: STRING
+  },
+  answer: {
+    id: INT,
+    result: BYTE,
+    score: INT,
+    hint: STRING
+  },
+  getHint: {
+    id: INT
+  },
+  hint: {
+    id: INT,
+    hint: STRING
+  },
+  exit: {
+    id: INT
+  },
+  ack: {
+    id: INT
+  },
+  error: {
+    id: INT,
+    error: STRING
+  },
+  heartbeat: {
+    id: INT
   }
 };
 var responses = {};
@@ -48,12 +87,55 @@ function parseMessage(message, messageType) {
     } else if (type === STRING) {
       var len = message.readInt16BE(offset);
       offset += 2;
-      result[key] = message.toString("utf8", offset, offset + len);
+      result[key] = read2ByteUnicode(message, offset, offset + len);
       offset += len;
-    } // else error
+    } else if (type === BYTE) {
+      result[key] = message.readUInt8(offset);
+      offset += 1;
+    }
   });
 
   return result;
+}
+
+function swapBytes(buffer, offset, end) {
+  offset = offset || 0;
+  end = end || buffer.length - offset;
+  // var l = buffer.length - offset;
+  // if (l & 0x01) {
+  //   throw new Error('Buffer length must be even');
+  // }
+  for (var i = offset; i <= end; i += 2) {
+    var a = buffer[i];
+    buffer[i] = buffer[i+1];
+    buffer[i+1] = a;
+  }
+  return buffer; 
+}
+
+function read2ByteUnicode(buffer, offset, end) {
+  offset = offset || 0;
+  end = end || buffer.length - offset;
+  var buf = Buffer.alloc(end - offset);
+  buffer.copy(buf, 0, offset, end);
+  buf = swapBytes(buf);
+  return buf.toString("utf16le");
+}
+
+function get2ByteUnicodeEncoding(value) {
+  var encoding = Buffer.alloc(2 + value.length * 2);
+  encoding.writeInt16BE(value.length * 2);
+  var offset = 2;
+  encoding.write(value, offset, value.length * 2, "utf16le");
+  encoding = swapBytes(encoding, offset);
+  return encoding;
+  // _.forEach(string, function (char) {
+  //   console.log(char);
+  //   buffer.writeUInt8(0, offset);
+  //   offset += 1;
+  //   buffer.write(char, offset);
+  //   offset += 1;
+  // });
 }
 
 function handleMessage(message, remote) {
@@ -84,13 +166,14 @@ function sendMessage(type, message) {
     var encoding;
     var value = message[key];
     if (type === STRING) {
-      encoding = Buffer.alloc(2 + value.length);
-      encoding.writeInt16BE(value.length);
-      encoding.write(value, 2);
+      encoding = get2ByteUnicodeEncoding(value);
     } else if (type === INT) {
       encoding = Buffer.alloc(2);
       encoding.writeInt16BE(value);
-    } // else error
+    } else if (type === BYTE) {
+      encoding = Buffer.alloc(1);
+      encoding.writeUInt8(value);
+    }
     encodings.push(encoding);
   });
 
@@ -101,6 +184,34 @@ function sendMessage(type, message) {
       //console.log("error: " + err);
     }
   });
+}
+
+function promptGame() {
+  var questions = [
+    {
+      name: "command",
+      type: "list",
+      message: "Choose a command: \nDefinition: " + gameDef.definition
+        + "\n" + "Hint: " + gameDef.hint + " (" + gameDef.hint.length + " characters)",
+      choices: [
+        {
+          name: "Get Hint",
+          value: "getHint"
+        }, {
+          name: "Guess",
+          value: "startGuess"
+        }, {
+          name: "New Game",
+          value: "startGame"
+        }, {
+          name: "Quit",
+          value: "quit"
+        }
+      ]
+    }
+  ];
+  
+  return inquirer.prompt(questions).then((command) => handleCommand(command.command));
 }
 
 commands.setConfig = function () {
@@ -123,6 +234,12 @@ commands.setConfig = function () {
   });
 };
 
+commands.getHint = function () {
+  sendMessage("getHint", {
+    id: gameDef.id
+  });
+}
+
 commands.startGame = function () {
   sendMessage("startGame", {
     aNum: "A02195862",
@@ -130,52 +247,74 @@ commands.startGame = function () {
     firstName: "Brett",
     alias: "BrettB"
   });
-  return q.resolve("Done");
 };
 
+commands.startGuess = function () {
+  return inquirer.prompt({
+    name: "guess",
+    message: "Guess:"
+  }).then(function (guess) {
+    sendMessage("guess", {
+      id: gameDef.id,
+      guess: guess.guess
+    });
+  });
+}
+
+commands.quit = function () {
+  sendMessage("exit", {
+    id: gameDef.id
+  });
+  process.exit();
+}
+
+handlers.answer = function (message) {
+  gameDef.hint = message.hint;
+  // TODO: show score, result?
+  promptGame();
+}
+
+handlers.heartbeat = function (message) {
+  sendMessage("ack", {
+    id: message.id
+  });
+}
+
+handlers.hint = function (message) {
+  gameDef.hint = message.hint;
+  promptGame();
+}
+
 handlers.gameDef = function (message) {
+  gameDef = message;
+  promptGame();
+};
+
+function getCommand(callback) {
   var questions = [
     {
       name: "command",
       type: "list",
-      message: "Definition: " + message.definition + "\n" + "Hint: " + message.hint,
+      message: "Choose a command:",
       choices: [
         {
-          name: "Get Hint",
-          value: "getHint"
+          name: "Start Game",
+          value: "startGame"
         }, {
-          name: "Guess",
-          value: "guess"
-        }, {
-          name: "Quit",
-          value: "quit"
+          name: "Set Config",
+          value: "setConfig"
         }
       ]
     }
   ];
-  
-  return inquirer.prompt(questions);//.then((command) => handleCommand(command.command));
-};
+
+  return inquirer.prompt(questions);
+}
 
 function handleCommand(command) {
   if (commands[command]) {
-    commands[command]().then(getCommand);
+    commands[command]();
   }
 }
 
-var prompts = new Rx.Subject();
-inquirer.prompt(prompts);
-prompts.onNext({
-  name: "command",
-  type: "list",
-  message: "Choose a command:",
-  choices: [
-    {
-      name: "Set Config",
-      value: "setConfig"
-    }, {
-      name: "Start Game",
-      value: "startGame"
-    }
-  ]
-});
+getCommand().then((command) => handleCommand(command.command));
